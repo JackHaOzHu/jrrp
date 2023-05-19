facd29d6a19fd1d66315b58634abf306452e5392
JackHaOzHu <113544193+JackHaOzHu@users.noreply.github.com>
2023-05-20 05:43:32 +0800

    更正一些低级错误

--- jrrp/config.py
+++ jrrp/config.py
@@ -10,46 +10,42 @@ class JrrpConfig(Serializable):
 	title: bool = False
 	command: List[str] = ["!!jrrp"]
 	message: List[dict] = [
-		{
-			"expr": "jrrp == 100",
-			"start": "！！！！！你今天的人品值是：",
-			"end": "！100！100！！！！！",
-			"title": True
-		},
-		{
-			"expr": "jrrp == 99",
-			"end": "！但不是 100……"
-		},
-		{
-			"expr": "jrrp >= 90",
-			"end": "！好评如潮！"
-		},
-		{
-			"expr": "jrrp >= 60",
-			"end": "！是不错的一天呢！"
-		},
-		{
-			"expr": "jrrp > 50",
-			"end": "！还行啦还行啦。"
-		},
-		{
-			"expr": "jrrp == 50",
-			"end": "！五五开……"
-		},
-		{
-			"expr": "jrrp >= 40",
-			"end": "！还……还行吧……？"
-		},
-		{
-			"expr": "jrrp >= 11",
-			"end": "！呜哇……"
-		},
-		{
-			"expr": "jrrp >= 1",
-			"end": "……（没错，是百分制）",
-		},
-		{
-			"expr": "True",
-			"end": "……"
-		}
+        	{
+ 	           "expr": "jrrp == 100",
+   	         "start": "！！！！！你今天的人品值是：",
+  	          "end": "！100！100！！！！！",
+	            "title": true
+	        },
+	        {
+	            "expr": "jrrp == 99",
+	            "end": "！但不是 100……"
+	        },
+	        {
+	            "expr": "jrrp >= 90 and jrrp < 99",
+  	          "end": "！好评如潮！"
+	        },
+ 	       {
+  	          "expr": "jrrp >= 60 and jrrp < 90",
+ 	           "end": "！是不错的一天呢！"
+ 	       },
+ 	       {
+  	          "expr": "jrrp > 50 and jrrp < 60",
+  	          "end": "！还行啦还行啦。"
+	        },
+   	     {
+ 	           "expr": "jrrp == 50",
+ 	           "end": "！五五开……"
+ 	       },
+ 	       {
+   	         "expr": "jrrp >= 40 and jrrp < 50",
+ 	           "end": "！还……还行吧……？"
+ 	       },
+	        {
+ 	           "expr": "jrrp >= 11 and jrrp < 40",
+	            "end": "！呜哇……"
+ 	       },
+ 	       {
+ 	           "expr": "jrrp >= 1 and jrrp <= 10",
+ 	           "end": "……（没错，是百分制）"
+  	      }
 	]
